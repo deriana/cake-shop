6d42570895386d5051a42d8ed969c0e802ef66db
Deryana <derana765@gmail.com>
2024-11-02 06:17:19 +0700

    add: templatin admin and auth login

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include_once("views/partials/header.php");
+?>
+
+<div class="az-content az-content-dashboard">
+    <div class="container">
+        <h1>Hello, World</h1>
+    </div>
+</div>
+
+<?php
+include_once("views/partials/footer.php");
+?>
---
+++ models/Database.php
@@ -0,0 +1,7 @@
+<?php
+define("HOSTNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "root");
+define("DB", "db_cake");
+
+$conn = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DB);
